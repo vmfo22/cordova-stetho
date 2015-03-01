1208dd580c918b4cec07db27648697d789945c27
Carlos Rosquillas <crosquillas@gmail.com>
2015-02-28 20:00:09 -0600

    Write AndroidManifest.xml

--- hooks/manifest.js
+++ hooks/manifest.js
@@ -5,10 +5,12 @@ module.exports = function(context) {
   var Q         = context.requireCordovaModule('q');
   var dfd       = new Q.defer();
   var parser    = new xml2js.Parser();
+  var builder   = new xml2js.Builder();
   var platforms = context.opts.platforms;
   var android   = false;
 
   var path;
+  var output;
 
   if (platforms) {
     platforms.forEach(function(value) {
@@ -23,12 +25,23 @@ module.exports = function(context) {
       parser.parseString(data, function(err, result) {
         var modified = result;
         var stetho = 'com.bridge.CDVStetho';
+
         modified.manifest.application[0].$['android:name'] = stetho;
+        output = builder.buildObject(modified);
+
+        fs.writeFile(path, output, function(err) {
+          if (err) {
+            console.log('cordova-stetho - Error writing AndroidManifest.xml');
+            console.log(err);
+          } else {
+            console.log('cordova-stetho - AndroidManifest.xml updated');
+          }
+        });
+
         dfd.resolve();
       });
     });
 
-    console.log('cordova-stetho - AndroidManifest.xml updated');
   }
 
   return dfd.promise;
