cab93739ad0317b06756fc022fc6bb337e391892
Carlos Rosquillas <crosquillas@gmail.com>
2015-02-28 19:15:37 -0600

    Read AndroidManifest.xml in hook

--- hooks/manifest.js
+++ hooks/manifest.js
@@ -1,9 +1,33 @@
 module.exports = function(context) {
-  var Q    = context.requireCordovaModule('q');
-  var dfd  = new Q.defer();
+  var fs     = require('fs');
+  var xml2js = require('xml2js');
 
-  dfd.resolve();
-  console.log('cordova-stetho - AndroidManifest.xml updated');
+  var Q         = context.requireCordovaModule('q');
+  var dfd       = new Q.defer();
+  var parser    = new xml2js.Parser();
+  var platforms = context.opts.platforms;
+  var android   = false;
+
+  var path;
+
+  if (platforms) {
+    platforms.forEach(function(value) {
+      if (value === 'android') { android = true; }
+    });
+  }
+
+  if (android) {
+    path = context.opts.projectRoot + '/platforms/android/AndroidManifest.xml';
+
+    fs.readFile(path, function(err, data) {
+      parser.parseString(data, function(err, result) {
+        console.dir(result);
+        dfd.resolve();
+      });
+    });
+
+    console.log('cordova-stetho - AndroidManifest.xml updated');
+  }
 
   return dfd.promise;
 };
