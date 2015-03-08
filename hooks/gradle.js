86542e360e4599a0d7acd657d316ecbb5bacb2ec
Carlos Rosquillas <crosquillas@gmail.com>
2015-03-07 22:01:51 -0600

    Use spawn rather than exec and handle errors

--- hooks/gradle.js
+++ hooks/gradle.js
@@ -1,13 +1,29 @@
 module.exports = function(context) {
-  var Q    = context.requireCordovaModule('q');
-  var exec = require('child_process').exec;
-  var dfd  = new Q.defer();
+  'use strict';
 
-  console.log('Installing "com.disusered.stetho" dependencies ');
-  exec('gradle getDeps', {
+  var Q     = context.requireCordovaModule('q');
+  var spawn = require('child_process').spawn;
+  var dfd   = new Q.defer();
+
+  console.log('Installing dependencies with Gradle');
+  var build = spawn('gradle', ['getDeps'], {
     cwd: context.opts.plugin.dir
-  }, function() {
-    dfd.resolve();
+  });
+
+  build.stdout.on('data', function(data) {
+    process.stdout.write(data);
+  });
+
+  build.stderr.on('data', function(data) {
+    process.stderr.write(data);
+  });
+
+  build.on('exit', function(code) {
+    if (code === 0) {
+      dfd.resolve();
+    } else {
+      dfd.reject();
+    }
   });
 
   return dfd.promise;
